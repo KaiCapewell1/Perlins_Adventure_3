d8b2e4978a0ad55c0a3303a4d7fb06310a5175e5
Kaicapewell1 <kaijcapewell@gmail.com>
2026-02-12 19:11:12 +0000

    forgot to git in a while a lot as changed

--- Assets/WorldGenScripts/GenerateMap.cs
+++ Assets/WorldGenScripts/GenerateMap.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GenerateMap : MonoBehaviour
@@ -9,37 +10,97 @@ public class GenerateMap : MonoBehaviour
     public Vector2Int randomStartRoom;
     public Vector2Int randomEndRoom;
     public GameObject TEMP_mapWall;
+
+
+    [Range(0, 100)]
+    public float densityPercent;
+
+    bool[,] occupied;
+    
+
+
+
+
+
     public void Start()
     {
+        occupied = new bool[mapwidth, mapheight];
+
         GenerateMapSize();
         DrawBackgroundGrid();
     }
     public void GenerateMapSize()
     {
-        int buffer = 7;
+        genrooms.Room_centers.Clear();
+        float density = densityPercent / 100f;
+        int targetCoverage = Mathf.RoundToInt((mapwidth * mapheight) * density);
 
-        randomStartRoom = new Vector2Int(Random.Range(2, mapwidth - buffer), Random.Range(2, mapheight - buffer));
-        genrooms.GenStartEndRoom(randomStartRoom);
 
-        randomEndRoom = new Vector2Int(Random.Range(2, mapwidth - 2), Random.Range(2, mapheight - 2));
-        genrooms.GenStartEndRoom(randomEndRoom);
+        int CurrentroomCount = 0;
+
+        while (CurrentroomCount < targetCoverage)
+        {
+            int width = Random.Range(10, 40);
+            int height = Random.Range(10, 40);
+
+            Vector2Int pos = new Vector2Int(
+                Random.Range(10, mapwidth - 10),
+                Random.Range(10, mapheight - 10)
+            );
+
+            if (!CanplaceRoom(pos, width, height, 4))
+                continue;
+
+            int roomArea = genrooms.GenerateRoom(pos, width, height, occupied);
+            CurrentroomCount += roomArea;
+        }
+
+        int randomRoom = Random.Range(0, genrooms.Room_centers.Count);
+
+        Vector2Int startroom = genrooms.Room_centers[randomRoom];
+
+        randomRoom = Random.Range(0, genrooms.Room_centers.Count);
+
+        Vector2Int EndRoom = genrooms.Room_centers[randomRoom];
+
+        genrooms.PlaceDoors(startroom, EndRoom);
+
     }
 
     public void DrawBackgroundGrid()
     {
-        for (int x = 0; x < mapwidth; x++)
-        {
-            for (int y = 0; y < mapheight; y++)
-            {
-               
+        for (int x = 0; x < mapwidth; x++) {
+            for (int y = 0; y < mapheight; y++){
                 bool isEdge = (x == 0 || x == mapwidth - 1 || y == 0 || y == mapheight - 1);
 
                 if (isEdge)
-                {
-                    GameObject tile = Instantiate(TEMP_mapWall, new Vector3(x, y, 0), Quaternion.identity);
-                    tile.transform.parent = this.transform;
-                }
+                { GameObject tile = Instantiate(TEMP_mapWall, new Vector3(x, y, 0), Quaternion.identity);
+                    tile.transform.parent = this.transform;}
             }
         }
+    }
+
+    public bool CanplaceRoom(Vector2Int center,int width, int height, int padding)
+    {
+        int halfWidth = width / 2;
+        int halfHeight = height / 2;
+
+        for (int x = -halfWidth - padding; x <= halfWidth + padding; x++)
+        {
+            for (int y = -halfHeight - padding; y <= halfHeight + padding; y++)
+            {
+                int checkX = center.x + x;
+                int checkY = center.y + y;
+
+                if (checkX < 0 || checkX >= mapwidth || checkY < 0 || checkY >= mapheight)
+                    return false;
+
+
+                if (occupied[checkX, checkY])
+                    return false;
+            }
+        }
+
+        return true;
     }
 }
--- Assets/WorldGenScripts/generateRooms.cs
+++ Assets/WorldGenScripts/generateRooms.cs
@@ -1,68 +1,149 @@
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEditor.Overlays;
 using UnityEngine;
+using UnityEngine.LightTransport;
 using UnityEngine.Tilemaps;
+using static UnityEditor.PlayerSettings;
 
 public class generateRooms : MonoBehaviour
 {
-    public Tilemap tilemap;
+    public Tilemap tilemapFloor;
+    public Tilemap tilemapWall;
 
     public Tile LeftWall;
     public Tile RightWall;
     public Tile TopWall;
     public Tile BottomWall;
-   
+
+    public Tile Floor;
+
     public GameObject Player;
-    public void GenStartEndRoom(Vector2Int randomStartRoom)
+
+    public GameObject EndDoorObj;
+    public GameObject StartDoorObj;
+
+
+    public List<Vector2Int> Room_centers = new List<Vector2Int>();
+    public int GenerateRoom(Vector2Int center, int roomWidth, int roomHeight, bool[,] occupied)
     {
-        int startroomRadius = 4;
-        //create a 5x5 room with corrasponding tile at the correct place
-      
-        for (int x  = -startroomRadius; x <= startroomRadius; x++)
+
+        int tilePlaced = 0;
+
+        int HalfW = roomWidth / 2;
+        int HalfH = roomHeight / 2;
+
+
+        for (int x = -HalfW; x <= HalfW; x++)
         {
-            for (int y = -startroomRadius; y <= startroomRadius; y++) 
+            for (int y = -HalfH; y <= HalfH; y++)
             {
-                Vector3Int tilePos = new Vector3Int(randomStartRoom.x + x, randomStartRoom.y + y, 0);
-                if (x == -startroomRadius || x == startroomRadius || y == -startroomRadius || y == startroomRadius)
-                {
-                    tilemap.SetTile(tilePos, BottomWall);
-                }
+
+                int WorldX = center.x + x;
+                int WorldY = center.y + y;
+
+                if (WorldX < 0 || WorldX >= occupied.GetLength(0) ||
+                   WorldY < 0 || WorldY >= occupied.GetLength(1))
+                    continue;
+
+
+
+                Vector3Int pos = new Vector3Int(WorldX, WorldY, 0);
+
+
+                bool isWall =
+                (x == -HalfW ||
+                 x == HalfW ||
+                 y == -HalfH ||
+                 y == HalfH);
+
+                if (isWall)
+                { tilemapWall.SetTile(pos, BottomWall); }
+
+                else
+                { tilemapWall.SetTile(pos, null); tilemapFloor.SetTile(pos, Floor); }
+
+                occupied[WorldX, WorldY] = true;
+
+                tilePlaced++;
+
+
             }
         }
 
-        //Generate the door
+        Room_centers.Add(center);
 
-        int side = Random.Range(0, 4);
+        return tilePlaced;
+    }
 
-        switch (side)
+    public void PlaceDoors(Vector2Int StartRoomCenter, Vector2Int EndRoomsCenter)
+    {
+
+        // --- Start Door ---
+        Vector3Int startPos = FindWallSpot(StartRoomCenter);
+        tilemapWall.SetTile(startPos, null);
+        tilemapFloor.SetTile(startPos, Floor);
+        Vector3 startCenteredPos = new Vector3(startPos.x + 0.5f, startPos.y + 1.5f, 0);
+        GameObject StartDoor = Instantiate(StartDoorObj, startCenteredPos, Quaternion.identity);
+        StartDoor.GetComponent<SpriteRenderer>().sortingOrder = 100;
+
+        // --- Spawn Player ---
+        Vector3 playerPosition = new Vector3(startCenteredPos.x, startCenteredPos.y - 5, 0);
+        Player.transform.position = playerPosition;
+
+        Random.Range(0, 100);
+
+        // --- End Door ---
+        Vector3Int EndDoorPos = FindWallSpot(EndRoomsCenter);
+        tilemapWall.SetTile(EndDoorPos, null);
+        tilemapFloor.SetTile(EndDoorPos, Floor);
+        Vector3 endCenteredPos = new Vector3(EndDoorPos.x + 0.5f, EndDoorPos.y + 1.5f, 0);
+        GameObject Endoor = Instantiate(EndDoorObj, endCenteredPos, Quaternion.identity);
+        Endoor.GetComponent<SpriteRenderer>().sortingOrder = 100;
+
+
+        
+    }
+
+
+    public Vector3Int FindWallSpot(Vector2Int RoomCenter)
+    {
+    
+        Vector3Int WallCheckUp = new Vector3Int(RoomCenter.x, RoomCenter.y, 0);
+
+
+        while (tilemapWall.GetTile(WallCheckUp) == null)
         {
-            case 0: //left
-                for(int i = -1; i <= 1; i++)
-                {
-                    tilemap.SetTile(new Vector3Int(randomStartRoom.x - startroomRadius, randomStartRoom.y + i, 0), null);
-                }
-                break;
-            case 1: // Right 
-                for (int i = -1; i <= 1; i++)
-                {
-                    tilemap.SetTile(new Vector3Int(randomStartRoom.x + startroomRadius, randomStartRoom.y + i, 0), null);
-                }
-                break;
+            WallCheckUp.y++;
+        }
 
-            case 2: // Top
-                for (int i = -1; i <= 1; i++)
-                {
-                    tilemap.SetTile(new Vector3Int(randomStartRoom.x + i, randomStartRoom.y + startroomRadius, 0), null);
-                }
-                break;
+        Vector3Int WallCheckLeft = new Vector3Int(RoomCenter.x, RoomCenter.y, 0);
+        while (tilemapWall.GetTile(WallCheckLeft) == null) { WallCheckLeft.x--; }
 
-            case 3: // Bottom 
-                for (int i = -1; i <= 1; i++)
-                {
-                    tilemap.SetTile(new Vector3Int(randomStartRoom.x + i, randomStartRoom.y - startroomRadius, 0), null);
-                }
+        Vector3Int WallCheckRight = new Vector3Int(RoomCenter.x, RoomCenter.y, 0);
+        while (tilemapWall.GetTile(WallCheckRight) == null) { WallCheckRight.x++; }
+
+   
+        int direction = Random.Range(0, 2);
+        int chosenSide = WallCheckUp.x;
+
+        switch (direction)
+        {
+            case 0:
+          
+                chosenSide = Random.Range(WallCheckLeft.x + 1, WallCheckUp.x);
+                break;
+            case 1:
+                chosenSide = Random.Range(WallCheckUp.x, WallCheckRight.x - 1);
                 break;
         }
 
-        Player.transform.position = new Vector3(randomStartRoom.x , randomStartRoom.y, 0);
+    
+        return new Vector3Int(chosenSide, WallCheckUp.y, 0);
     }
+   
+    
 }
 
